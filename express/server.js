7f10a003aca77eaca25454f5eea3b204e7ef9fa9
Sonny Lazuardi Hermawan <sonny.lazuardi@grabtaxi.com>
2019-09-18 13:32:12 +0800

    add counter

--- express/server.js
+++ express/server.js
@@ -4,6 +4,12 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
+let counter = 0;
+
+setInterval(() => {
+  counter++;
+}, 2000);
+
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -11,10 +17,11 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/counter', (req, res) => res.json({ counter }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/.netlify/functions/server', router); // path must route to lambda
 
 module.exports = app;
 module.exports.handler = serverless(app);
